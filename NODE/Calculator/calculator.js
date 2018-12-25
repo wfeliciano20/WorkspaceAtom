6c006e1dd322765829ccb2d33ad08940db5eda07
wiliam feliciano <wfeliciano@wiliams-MacBook-Pro.local>
2018-12-25 16:45:05 -0500

    calculator.js update with logic for website

--- NODE/Calculator/calculator.js
+++ NODE/Calculator/calculator.js
@@ -2,11 +2,27 @@
 
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
 
-    res.send("Hello world!");
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+
+    let num1 = Number(req.body.num1);
+
+    let num2 = Number(req.body.num2);
+
+    let result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
 
 });
 
