40223924314d6f41f565725cfb53c02c8d4fd111
wiliam feliciano <wfeliciano@wiliams-MacBook-Pro.local>
2018-12-28 22:11:50 -0500

    index js html update

--- Bitcoin-Ticker/index.js
+++ Bitcoin-Ticker/index.js
@@ -8,6 +8,12 @@ const app = espress();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function(req, req) {
+
+    res.sendFile(__dirname + "/index.html");
+
+})
+
 app.listen(3000, function() {
 
     console.log("server is running on port 3000");
