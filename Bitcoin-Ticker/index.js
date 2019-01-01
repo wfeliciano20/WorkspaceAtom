b3d39e0f17020ffc937d53797454c806f4cc6a14
wiliam feliciano <wfeliciano@wiliams-MacBook-Pro.local>
2019-01-01 17:06:47 -0500

    comment

--- Bitcoin-Ticker/index.js
+++ Bitcoin-Ticker/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
-const request = require("request");
+const request = require("request"); // use to do a request to an external server
 
 const app = espress();
 
