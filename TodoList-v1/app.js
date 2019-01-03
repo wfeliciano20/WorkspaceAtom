4979df2dcb11a1d03438476a88b50271c18a200e
wiliam feliciano <wfeliciano@wiliams-MacBook-Pro.local>
2019-01-03 00:52:28 -0500

    views folder ejs file and app.js update

--- TodoList-v1/app.js
+++ TodoList-v1/app.js
@@ -8,17 +8,71 @@ const request = require("request"); // use to do a request to an external server
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
-
-    extended: true
-
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("view engine", "ejs");
 
 app.get("/", function(req, res) {
 
-    res.sendFile(__dirname + "/index.html");
+    let today = new Date();
+
+    let currentDay = today.getDay();
+
+    let day = "";
+
+    switch (currentDay) {
+
+        case 0:
+
+            day = "Sunday";
+
+            break;
+
+        case 1:
+
+            day = "Monday";
+
+            break;
+
+        case 2:
+
+            day = "Tuesday";
+
+            break;
+
+        case 3:
+
+            day = "Wednesday";
+
+            break;
+
+        case 4:
+
+            day = "Thursday";
+
+            break;
+
+        case 5:
+
+            day = "Friday";
+
+            break;
+
+        case 6:
+
+            day = "Saturday";
+
+            break;
+
+        default:
+
+            console.log("Error: current day is equal to: " + currentDay);
+
+            break;
+
+    }
+
+    res.render("list", { kindOfDay: day });
 
 });
 
