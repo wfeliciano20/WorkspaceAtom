17d783595bfbfef18ae85a58cabfd5a803f8543d
wiliam feliciano <wfeliciano@wiliams-MacBook-Pro.local>
2019-02-18 16:54:10 -0500

    mongodb driver file separation

---
+++ FruitsProject/app_without_mongoose.js
@@ -0,0 +1,87 @@
+// How to do things without mongoose
+
+const MongoClient = require('mongodb').MongoClient;
+
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'fruitsDB';
+
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+client.connect(function(err) {
+
+    assert.equal(null, err);
+
+    console.log("Connected succesfully to server");
+
+    const db = client.db(dbName);
+
+    findDocuments(db, function() {
+
+        client.close();
+
+    });
+
+});
+
+const insertDocuments = function(db, callback) {
+
+    //Get the documents collection
+    const collection = db.collection('fruits');
+
+    //Insert some documents
+    collection.insertMany([
+
+        {
+            name: "Apple",
+            score: 8,
+            review: "Great fruit"
+        },
+        {
+            name: "Orange",
+            score: 6,
+            review: "Not so sweet"
+        },
+        {
+            name: "Banana",
+            score: 9,
+            review: "Great stuff!"
+        }
+
+    ], function(err, result) {
+
+        assert.equal(err, null);
+
+        assert.equal(3, result.result.n);
+
+        assert.equal(3, result.ops.length);
+
+        console.log("Inserted 3 documents into the collecttion");
+
+        callback(result);
+
+    });
+
+};
+
+const findDocuments = function(db, callback) {
+
+    //Get the documents collection
+    const collection = db.collection('fruits');
+
+    //Find some documents
+    collection.find({}).toArray(function(err, fruits) {
+
+        assert.equal(err, null);
+
+        console.log("Found the following records");
+
+        console.log(fruits);
+
+        callback(fruits);
+
+    });
+
+};
